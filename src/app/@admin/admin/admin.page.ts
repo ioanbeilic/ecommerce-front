d6ddc949a783ded3157c87c56f5c5812b07c14ee
ioan beilic <ioanbeilic@gmail.com>
2021-01-02 20:09:10 +0100

    add left menu to base

--- src/app/@admin/admin/admin.page.ts
+++ src/app/@admin/admin/admin.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.page.html',
-  styleUrls: ['./admin.page.scss'],
+  selector: "app-admin",
+  templateUrl: "./admin.page.html",
+  styleUrls: ["./admin.page.scss"],
 })
 export class AdminPage implements OnInit {
+  appPages = [
+    {
+      title: "Schedule",
+      url: "/app/tabs/schedule",
+      icon: "calendar",
+    },
+    {
+      title: "Speakers",
+      url: "/app/tabs/speakers",
+      icon: "people",
+    },
+    {
+      title: "Map",
+      url: "/app/tabs/map",
+      icon: "map",
+    },
+    {
+      title: "About",
+      url: "/app/tabs/about",
+      icon: "information-circle",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
